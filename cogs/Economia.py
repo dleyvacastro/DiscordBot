f978f9e744071465669978d6e60620ad1024372a
dleyvacastro <dleyvacastro@gmail.com>
2021-06-13 00:01:13 -0500

    New economy system

--- cogs/Economia.py
+++ cogs/Economia.py
@@ -2,8 +2,9 @@ import discord
 import asyncio
 from discord.ext import commands, tasks
 from discord.utils import get
-from replit import db
+from replit import Database
 
+db = Database('https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjM2Mzg3NDcsImlhdCI6MTYyMzUyNzE0NywiaXNzIjoiY29ubWFuIiwiZGF0YWJhc2VfaWQiOiI3OGI3YzQxMC0zNGM3LTQxNzAtYmRkNS03ZDcxMzNhODU5NTkifQ.h4ToqqoRxDAhDd8SeQ5_OPuLJzXkTlKDQWEP23HfxfJuoIFFqc0XWKALQmTeIQjya1HV2TTFYibngDJM0k1tAg')
 
 
 class Economia(commands.Cog):
@@ -15,17 +16,30 @@ class Economia(commands.Cog):
 
         self.miembros_id = ['394982506417487872', '393592731420721154', '318611546383319041',
                             '396305256377614337', '704883078291521537', '528019938196324372']
-        #precios
+        # precios
 
         self.chnick_p = 20
+        self.censura_p = 10
+        self.tts_p = 5
+        self.golpe_de_estado_p = 6402
 
-    @tasks.loop(hours = 24)
+    def get_user_money(self, user):
+        v = int(db[f'{user.id}'])
+        return v
+
+    def cobro(self, author, precio):
+        v = self.get_user_money(author)
+        if v > precio:
+            db[f'{author.id}'] = v - precio
+            return True
+        return False
+
+    @tasks.loop(hours=24)
     async def give_tokens(self):
         for i in self.miembros_id:
             db[f"{i}"] = f'{int(db[f"{i}"]) + 1}'
             print(db[f"{i}"])
         await self.pruebas_c.send('Se han recargado los tokens de los miembros correspondientes.')
-        
 
     @give_tokens.before_loop
     async def before_give_tokens(self):
@@ -38,7 +52,7 @@ class Economia(commands.Cog):
             self.miembros.append(miembro)
         keys = db.keys()
         print(keys)
-    
+
     @commands.command()
     @commands.has_role(849454598296830023)
     async def reset_economy(self, ctx, message):
@@ -49,50 +63,97 @@ class Economia(commands.Cog):
         else:
             await ctx.send('Por favor confime la orden.')
 
-    def get_money(self, author):
-        v = db[str(author)]
-        return v
-
     @commands.command()
     async def money(self, ctx, member: commands.MemberConverter = None):
         if member == None:
             member = ctx.author
-        await ctx.reply(f'Maldito asalariado usted tiene: {self.get_money(member.id)} Dorian Coins.')
+        await ctx.reply(f'Maldito asalariado usted tiene: {self.get_user_money(member)} Dorian Coins.')
 
     @commands.command()
     @commands.has_role(849454598296830023)
-    async def add_money(self, ctx, member: commands.MemberConverter = None, amount = 1):
+    async def add_money(self, ctx, member: commands.MemberConverter = None, amount=1):
         if member == None:
             member = ctx.author
         id = str(member.id)
         db[id] = f'{int(db[id])+amount}'
-        await ctx.reply(f'Maldito asalariado, se le ha depositado {amount}$ en su cuenta, ahora usted tiene: {self.get_money(ctx.author.id)} Dorian Coins.')
+        await ctx.reply(f'Maldito asalariado, se le ha depositado {amount}$ en su cuenta, ahora usted tiene: {self.get_user_money(ctx.author)} Dorian Coins.')
 
     @commands.command()
     @commands.has_role(849454598296830023)
-    async def rest_money(self, ctx, member: commands.MemberConverter = None, amount = 1):
+    async def rest_money(self, ctx, member: commands.MemberConverter = None, amount=1):
         if member == None:
             member = ctx.author
         id = str(member.id)
         db[id] = f'{int(db[id])-amount}'
-        await ctx.reply(f'Maldito asalariado, se le ha retirado {amount}$ de su cuenta, ahora usted tiene: {self.get_money(ctx.author.id)} Dorian Coins.')
+        await ctx.reply(f'Maldito asalariado, se le ha retirado {amount}$ de su cuenta, ahora usted tiene: {self.get_user_money(member)} Dorian Coins.')
 
     @commands.command()
-    async def send(self, ctx, member: commands.MemberConverter, amount = 1):
+    async def send(self, ctx, member: commands.MemberConverter, amount: int = 1):
         id = str(member.id)
         a_id = str(ctx.author.id)
+        if int(db[a_id]) < amount:
+            await ctx.reply('Hoy no se fia, mañana si.')
+            return
         db[a_id] = f'{int(db[a_id])-amount}'
         db[id] = f'{int(db[id])+amount}'
         await ctx.reply(f'Transfrencia exitosa. Se han enviado {amount}$ a {member.mention}.')
 
+    # Comandos compras.
+    @commands.command(pass_context=True)
+    async def chnick(self, ctx, member: commands.MemberConverter, *, nick):
+        if self.cobro(ctx.author, self.chnick_p):
+            await member.edit(nick=nick)
+            await ctx.send(f'{ctx.author.mention} ha redimido un cambio de apodo para {member.nick} por {self.chnick_p}, ahora tienes {db[str(ctx.author.id)]}.')
+            await ctx.send(f'Ahora seras conocido como: {member.mention} mamahuevo.')
+        else:
+            await ctx.send(f'Trabaje vago pobre')
+
+    @commands.command(aliases=['RCN', 'Caracol', 'censura'])
+    async def clear(self, ctx, amount=2):
+        """
+        Alias: RCN, Caracol, Censura
+        """
+        if self.cobro(ctx.author, self.censura_p):
+            await ctx.channel.purge(limit=amount)
+            if amount > 10:
+                await ctx.send(f'Premio Juan Pablo Bieri al Censurador del año, ¿Usted mató a Jaime Garzon o q? {ctx.author.mention}')
+            # await ctx.author.edit(nick="Dr. MinTIC")
+        else:
+            await ctx.send('Trabaje vago mamamerto.')
+
     @commands.command()
-    async def compra(self, ctx, parameter, member: commands.MemberConverter, nick):
-        v = db[f'{ctx.author.id}']
-        if parameter == 'chnick'  and v >= self.chnick_p:
-            db[f'{ctx.author.id}'] = int(db[f'{ctx.author.id}'])
-            await member.edit(nick = nick)
-            await ctx.send(f'{member.name} Ahora seras conocido como {member.mention} mamahuevo.')            
-        
+    async def tts(self, ctx, *, message):
+        if self.cobro(ctx.author, self.tts_p):
+            await ctx.send(ctx.author.mention)
+            await ctx.send(message, tts=1)
+            await ctx.message.delete()
+        else:
+            await ctx.send('Trabaje vago mamerto')
+
+    @commands.command()
+    async def golpe_de_estado(self, ctx):
+        miembros = []
+        admin_role = get(ctx.guild.roles, id=685973595423375388)
+        firmes_role = get(ctx.guild.roles, id=685974684134801457)
+        anuncio = self.bot.get_channel(839554918561611837)
+        await ctx.send(f'{ctx.author.mention} quiere tomar el poder por la fuerza')
+        if self.cobro(ctx.author, self.golpe_de_estado_p):
+            await ctx.send('Y tiene el capital para lograrlo. Felicidades nuevo admin.')
+            await anuncio.send(f'El nuevo admin es (golpe de estado): {ctx.author.mention}')
+            await anuncio.send(file=discord.File('./images/admin.jpg'))
+            for i in db.keys():
+                miembro = await ctx.guild.fetch_member(i)
+                miembros.append(miembro)
+            for i in miembros:
+                if i.id == ctx.author.id:
+                    await i.add_roles(admin_role)
+                    await i.remove_roles(firmes_role)
+                else:
+                    await i.add_roles(firmes_role)
+                    await i.remove_roles(admin_role)
+        else:
+            await ctx.send('Pero no tiene los recursos suficientes, buen intento.')
+
 
 def setup(bot):
     bot.add_cog(Economia(bot))
--- cogs/Moderacion.py
+++ cogs/Moderacion.py
@@ -4,33 +4,34 @@ from discord.ext import commands
 from discord.utils import get
 from statistics import mode
 
+
 class Moderacion(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.zawarudo = False
 
-    @commands.command(aliases = ['RCN', 'Caracol', 'Censura', 'censura'])
+    @commands.command()
     @commands.has_any_role(685973595423375388, 849454598296830023)
-    async def clear(self, ctx, amount = 2):
+    async def Clear(self, ctx, amount=2):
         """
         Alias: RCN, Caracol, Censura
         """
-        await ctx.channel.purge(limit = amount)
+        await ctx.channel.purge(limit=amount)
         if amount > 10:
             await ctx.send(f'Premio Juan Pablo Bieri al Censurador del año, ¿Usted mató a Jaime Garzon o q? {ctx.author.mention}')
             # await ctx.author.edit(nick="Dr. MinTIC")
-    
+
     @commands.command()
     @commands.has_any_role(685973595423375388, 849454598296830023)
-    async def kick(self, ctx, member : commands.MemberConverter, *, reason = None):
-        await member.kick(reason = reason)
+    async def kick(self, ctx, member: commands.MemberConverter, *, reason=None):
+        await member.kick(reason=reason)
         await ctx.send(f'Se comenta que a alias {member.mention} lo enterraron por sapo.')
 
     @commands.command()
     @commands.has_any_role(685973595423375388, 849454598296830023)
-    async def ban(self, ctx, member : commands.MemberConverter, *, reason = None):
-        await member.ban(reason = reason)
+    async def ban(self, ctx, member: commands.MemberConverter, *, reason=None):
+        await member.ban(reason=reason)
         await ctx.send(f'No debiste incordiar al Señor Tenebroso {member.mention}.')
 
     @commands.command()
@@ -45,55 +46,57 @@ class Moderacion(commands.Cog):
                 await ctx.guild.unban(user)
                 await ctx.send(f'Bienvenido de nuevo hermanito bolivariano {user.mention}.')
                 return
-    
+
     @commands.command(pass_context=True)
     @commands.has_any_role(685973595423375388, 849454598296830023)
-    async def chnick(self, ctx, member: commands.MemberConverter, *, nick):
+    async def force_chnick(self, ctx, member: commands.MemberConverter, *, nick):
         await member.edit(nick=nick)
         await ctx.send(f'Ahora seras conocido como: {member.mention} mamahuevo')
-    
+
     @commands.command()
     @commands.has_any_role(685973595423375388, 849454598296830023)
     async def zawarudo(self, ctx):
         tts = True
-        dio_time = ['Ni', 'San', 'Yon', 'Go', 'Roku', 'Nana', 'Hachi', 'Kyuu', 'Jyuu']
+        dio_time = ['Ni', 'San', 'Yon', 'Go',
+                    'Roku', 'Nana', 'Hachi', 'Kyuu', 'Jyuu']
         embed = discord.Embed(
-            title = 'Yare Yare Daze',
-            description = 'Estan como muy aletosos.',
-            colour = discord.Colour.random()
+            title='Yare Yare Daze',
+            description='Estan como muy aletosos.',
+            colour=discord.Colour.random()
         )
-        embed.set_image(url='https://media1.tenor.com/images/3546df32c4974a720904643ecf5956ef/tenor.gif?itemid=17040017')
-        await ctx.send(embed = embed)
+        embed.set_image(
+            url='https://media1.tenor.com/images/3546df32c4974a720904643ecf5956ef/tenor.gif?itemid=17040017')
+        await ctx.send(embed=embed)
         self.zawarudo = True
         self.channel_stoped = ctx.channel
 
         try:
             channel = ctx.author.voice.channel
-            self.voice = get(self.bot.voice_clients, guild = ctx.guild)
+            self.voice = get(self.bot.voice_clients, guild=ctx.guild)
             tts = False
             if self.voice and self.voice.is_connected():
                 await self.voice.move_to(channel)
             else:
                 self.voice = await channel.connect()
 
-            self.voice.play(discord.FFmpegPCMAudio("./audios/zawarudo.mp3"), after = lambda e: print(f'has finished playing'))
+            self.voice.play(discord.FFmpegPCMAudio(
+                "./audios/zawarudo.mp3"), after=lambda e: print(f'has finished playing'))
             self.voice.source = discord.PCMVolumeTransformer(self.voice.source)
             self.voice.source.volume = 0.5
-            await ctx.send('Zawardo!, Tomare Toki wo', tts = tts)
+            await ctx.send('Zawardo!, Tomare Toki wo', tts=tts)
             await asyncio.sleep(5)
         except:
-            await ctx.send('Zawardo!, Tomare Toki wo', tts = tts)       
-            await asyncio.sleep(1)     
-        
+            await ctx.send('Zawardo!, Tomare Toki wo', tts=tts)
+            await asyncio.sleep(1)
 
         m = await ctx.send('Ichi byou keika')
         for i in dio_time:
             await asyncio.sleep(2)
-            await m.edit(content = i+' byou keika')
+            await m.edit(content=i+' byou keika')
         await asyncio.sleep(1.5)
-        m2 = await ctx.send('Toki ua ugoki des', tts = tts)
+        m2 = await ctx.send('Toki ua ugoki des', tts=tts)
         await asyncio.sleep(1.5)
-        await m2.edit(content = 'Toki wa ugoki dasu')
+        await m2.edit(content='Toki wa ugoki dasu')
         self.zawarudo = False
 
     @commands.Cog.listener('on_message')
@@ -101,11 +104,9 @@ class Moderacion(commands.Cog):
         if ctx.author == self.bot.user:
             return
         if self.zawarudo and ctx.channel == self.channel_stoped:
-            await ctx.channel.purge(limit = 1)
-
-    
-
+            await ctx.channel.purge(limit=1)
 
 
 def setup(bot):
     bot.add_cog(Moderacion(bot))
+
